54de3197d2b0eb4e097f4c4d4985d00f07bbdd21
mitohato <mitohato14@gmail.com>
2018-11-07 17:03:00 +0900

    Add project

---
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ict.ryokon.shugakuryokon2018
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
