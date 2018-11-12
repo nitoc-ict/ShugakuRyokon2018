f3a89d2c0a8412469470a0e22a0bc0ffd13214c9
mosmosyrc <mi151327@edu.okinawa-ct.ac.jp>
2018-11-12 15:52:56 +0900

    declare userDataList and function addTarget()

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/target/RollCallTargetSettingViewModel.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/target/RollCallTargetSettingViewModel.kt
@@ -1,5 +1,12 @@
 package com.ict.ryokon.shugakuryokon2018.ui.setting.target
 
 import androidx.lifecycle.ViewModel
+import com.ict.ryokon.shugakuryokon2018.UserData
 
-class RollCallTargetSettingViewModel : ViewModel()
+class RollCallTargetSettingViewModel : ViewModel() {
+    lateinit var userDataList: ArrayList<UserData>
+
+    fun addTarget() {
+        //TODO iikanji ni Target ga tsuika sareta toki no syori wo kaku
+    }
+}
