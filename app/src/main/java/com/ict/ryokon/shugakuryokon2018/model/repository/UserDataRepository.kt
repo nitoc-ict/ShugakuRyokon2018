c4f9faba4f4e63b76f6bc6da881af6c7655ff60a
mitohato <mitohato14@gmail.com>
2018-11-14 15:26:52 +0900

    Add method what find by attendance number

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/model/repository/UserDataRepository.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/model/repository/UserDataRepository.kt
@@ -1,5 +1,6 @@
 package com.ict.ryokon.shugakuryokon2018.model.repository
 
+import com.ict.ryokon.shugakuryokon2018.model.AttendanceNumber
 import com.ict.ryokon.shugakuryokon2018.model.Minor
 import com.ict.ryokon.shugakuryokon2018.model.Name
 import com.ict.ryokon.shugakuryokon2018.model.UserData
@@ -16,4 +17,7 @@ object UserDataRepository {
     fun findByName(name: Name): UserData? = userDataList.find { it.name == name }
 
     fun findByMinor(minor: Minor): UserData? = userDataList.find { it.minor == minor }
+
+    fun findByAttendanceNumber(number: AttendanceNumber): UserData? =
+        userDataList.find { it.attendanceNumber ==  number}
 }
