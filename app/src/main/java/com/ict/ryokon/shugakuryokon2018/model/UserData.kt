82678c3d40b8e0bdddffe9576d112db29b77a020
mitohato <mitohato14@gmail.com>
2018-11-14 15:25:19 +0900

    Add AttendanceNumber to UserData

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/model/UserData.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/model/UserData.kt
@@ -2,6 +2,7 @@ package com.ict.ryokon.shugakuryokon2018.model
 
 data class UserData(
     var name: Name,
+    var attendanceNumber: AttendanceNumber,
     var minor: Minor,
     var isRollCallTarget: Boolean,
     var isAttend: Boolean
