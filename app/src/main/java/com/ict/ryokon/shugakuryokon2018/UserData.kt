3b011d44f022e6af8d3f370a0954d61dc3619dc6
mitohato <mitohato14@gmail.com>
2018-11-14 15:04:35 +0900

    Fix error

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/UserData.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/UserData.kt
@@ -2,7 +2,7 @@ package com.ict.ryokon.shugakuryokon2018
 
 data class UserData(
     var name: Name,
-    var minor: Int,
+    var minor: Minor,
     var isRollCallTarget: Boolean,
     var isAttend: Boolean
 )
