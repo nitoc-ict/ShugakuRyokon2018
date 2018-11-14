c13a25299287c17d744d9e390d26a8fb5dafb0ff
mitohato <mitohato14@gmail.com>
2018-11-14 15:23:05 +0900

    Change minor type

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/model/Minor.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/model/Minor.kt
@@ -2,4 +2,4 @@ package com.ict.ryokon.shugakuryokon2018.model
 
 import com.ict.ryokon.shugakuryokon2018.utils.Identifier
 
-class Minor(value: String) : Identifier<String>(value)
+class Minor(value: Int) : Identifier<Int>(value)
