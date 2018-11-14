ee5f1d36c33b018f46ef7df877e9adc57eaf2417
mitohato <mitohato14@gmail.com>
2018-11-14 14:53:30 +0900

    Add name class

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/UserData.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/UserData.kt
@@ -1,7 +1,7 @@
 package com.ict.ryokon.shugakuryokon2018
 
 data class UserData(
-    var name: String,
+    var name: Name,
     var minor: Int,
     var isRollCallTarget: Boolean,
     var isAttend: Boolean
