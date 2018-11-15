1d44069e5ef7a819c3e335d4ab9148e11eaf2451
mitohato <mitohato14@gmail.com>
2018-11-15 20:38:19 +0900

    Add method bind

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/view/RollCallViewHolder.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/view/RollCallViewHolder.kt
@@ -9,6 +9,7 @@ import com.ict.ryokon.shugakuryokon2018.model.UserData
 class RollCallViewHolder(private val binding: RowRollCallBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(userData: UserData) {
+        binding.userdata = userData
     }
 
     fun unbind() {
