0332fa16ecb60626421cf291369e922cd180de1c
mitohato <mitohato14@gmail.com>
2018-11-21 22:02:59 +0900

    Ask enable bluetooth when disable blurtooth

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/MainActivity.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.ict.ryokon.shugakuryokon2018.ui
 
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.ict.ryokon.shugakuryokon2018.R
@@ -10,6 +13,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bluetoothManager= getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+
+        if (!bluetoothAdapter.isEnabled) {
+            AlertDialog.Builder(this)
+                .setTitle("確認")
+                .setMessage("Bluetooth機能をONにしました")
+                .setPositiveButton("OK") { _, _ ->
+                }
+                .show()
+            bluetoothAdapter.enable()
+        }
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.navhost).navigateUp()
