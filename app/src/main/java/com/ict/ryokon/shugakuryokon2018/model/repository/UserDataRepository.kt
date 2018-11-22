58f29ff350d7fe01fec08ca9754fdaa356e8079f
mitohato <mitohato14@gmail.com>
2018-11-22 15:56:27 +0900

    Add user data for dummy

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/model/repository/UserDataRepository.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/model/repository/UserDataRepository.kt
@@ -6,7 +6,7 @@ import com.ict.ryokon.shugakuryokon2018.model.Name
 import com.ict.ryokon.shugakuryokon2018.model.UserData
 
 object UserDataRepository {
-    private var userDataList: ArrayList<UserData> = arrayListOf()
+    private var userDataList: ArrayList<UserData> = dummy()
 
     fun storeAll(userDataList: ArrayList<UserData>) {
         this.userDataList = userDataList
@@ -28,4 +28,110 @@ object UserDataRepository {
             }
         }
     }
+
+//    ダミー用のデータ(本番前削除)
+//    名前は[https://namegen.jp]にて生成
+    private fun dummy(): ArrayList<UserData> = arrayListOf(
+        UserData(
+            name = Name(
+                "山本 浩司",
+                "やまもと こうじ"
+            ),
+            attendanceNumber = AttendanceNumber(1),
+            minor = Minor(1),
+            isAttend = false,
+            isRollCallTarget = false
+        ),
+        UserData(
+            name = Name(
+                "丸岡 恭平",
+                "まるおか きょうへい"
+            ),
+            attendanceNumber = AttendanceNumber(2),
+            minor = Minor(2),
+            isAttend = false,
+            isRollCallTarget = false
+        ),
+        UserData(
+            name = Name(
+                "八重樫 良平",
+                "やえがし りょうへい"
+            ),
+            attendanceNumber = AttendanceNumber(3),
+            minor = Minor(3),
+            isAttend = false,
+            isRollCallTarget = false
+        ),
+        UserData(
+            name = Name(
+                "叶 優太",
+                "かのう ゆうた"
+            ),
+            attendanceNumber = AttendanceNumber(4),
+            minor = Minor(4),
+            isAttend = false,
+            isRollCallTarget = false
+        ),
+        UserData(
+            name = Name(
+                "岩月 拓人",
+                "いわつき たくと"
+            ),
+            attendanceNumber = AttendanceNumber(5),
+            minor = Minor(5),
+            isAttend = false,
+            isRollCallTarget = false
+        ),
+        UserData(
+            name = Name(
+                "片桐 志乃",
+                "かたぎり しの"
+            ),
+            attendanceNumber = AttendanceNumber(6),
+            minor = Minor(6),
+            isAttend = false,
+            isRollCallTarget = false
+        ),
+        UserData(
+            name = Name(
+                "花田 知香",
+                "はなだ ちか"
+            ),
+            attendanceNumber = AttendanceNumber(7),
+            minor = Minor(7),
+            isAttend = false,
+            isRollCallTarget = false
+        ),
+        UserData(
+            name = Name(
+                "島村 久恵",
+                "しまむら ひさえ"
+            ),
+            attendanceNumber = AttendanceNumber(8),
+            minor = Minor(8),
+            isAttend = false,
+            isRollCallTarget = false
+        ),
+        UserData(
+            name = Name(
+                "仲村 みづき",
+                "なかむら みづき"
+            ),
+            attendanceNumber = AttendanceNumber(9),
+            minor = Minor(9),
+            isAttend = false,
+            isRollCallTarget = false
+        ),
+        UserData(
+            name = Name(
+                "Julie Williams Wright",
+                "ジュリー ウィリアムズ ライト"
+            ),
+            attendanceNumber = AttendanceNumber(10),
+            minor = Minor(10),
+            isAttend = false,
+            isRollCallTarget = false
+        )
+    )
+
 }
