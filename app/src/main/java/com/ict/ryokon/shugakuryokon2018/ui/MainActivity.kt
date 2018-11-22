d190253bdff7128ffb480f25fab709239bacf183
mitohato <mitohato14@gmail.com>
2018-11-22 14:11:10 +0900

    Set menu in toolbar

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/MainActivity.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ict.ryokon.shugakuryokon2018.ui
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.ict.ryokon.shugakuryokon2018.R
@@ -13,4 +14,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.navhost).navigateUp()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(
+            R.menu.menu_main,
+            menu
+        )
+        return true
+    }
 }
