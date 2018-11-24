ceb160f9c6aa9b462599575358d73bb896a9003d
mitohato <mitohato14@gmail.com>
2018-11-24 23:15:58 +0900

    Fix databinding error

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/target/RollCallTargetSettingBindingModel.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/target/RollCallTargetSettingBindingModel.kt
@@ -1,11 +1,15 @@
 package com.ict.ryokon.shugakuryokon2018.ui.setting.target
 
 import android.util.Log
+import android.view.View
 import com.ict.ryokon.shugakuryokon2018.model.UserData
 import com.ict.ryokon.shugakuryokon2018.model.repository.UserDataRepository
 
 class RollCallTargetSettingBindingModel(val userData: UserData) {
-    fun onCheckedChanged(checked: Boolean) {
+    fun onCheckedChanged(
+        view: View,
+        checked: Boolean
+    ) {
         when (checked) {
             true -> {
                 addTarget()
