ce2ee1b417968ab9bcd4ff2338016ce933fac32f
Gatyamin <heyapple7orange7@gmail.com>
2018-11-29 12:15:51 +0900

    BeaconSettingViewModelとfragment_beacon_setting.xml変更

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/beacon/BeaconSettingViewModel.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/beacon/BeaconSettingViewModel.kt
@@ -1,14 +1,33 @@
 package com.ict.ryokon.shugakuryokon2018.ui.setting.beacon
 
 import android.os.Bundle
+import androidx.annotation.IntegerRes
 import androidx.lifecycle.ViewModel
+import com.ict.ryokon.shugakuryokon2018.model.Minor
+import com.ict.ryokon.shugakuryokon2018.model.UserData
+import com.ict.ryokon.shugakuryokon2018.model.repository.UserDataRepository
 
-class BeaconSettingViewModel : ViewModel(){
-    fun removeOnClick(savedInstanceState: Bundle?){
+class BeaconSettingViewModel : ViewModel() {
+    lateinit var userdata:UserData //lateinit今初期化しない
+    var minor:String = ""
 
+    fun removeOnClick(savedInstanceState: Bundle?) {
+        //前の画面にもどる
 
     }
-    fun changeOnClick(){
 
+    fun changeOnClick() {
+        //UserDataのMinorをuserdataにセットする
+        userdata.minor = Minor(Integer.parseInt(minor))
+        UserDataRepository.updateUserData(userdata)
+    }
+
+    fun onMinorChanged(
+        c: CharSequence,
+        start: Int,
+        before: Int,
+        count: Int
+    ) {
+        minor = c.toString()
     }
 }
