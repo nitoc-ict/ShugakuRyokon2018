faa86403ddb1da3b08eb3a3c2a819b47bfbd410d
Gatyamin <heyapple7orange7@gmail.com>
2018-11-29 11:17:53 +0900

    ビーコン設定画面の変更

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/beacon/BeaconSettingFragment.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/beacon/BeaconSettingFragment.kt
@@ -1,11 +1,33 @@
 package com.ict.ryokon.shugakuryokon2018.ui.setting.beacon
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.ict.ryokon.shugakuryokon2018.databinding.FragmentBeaconSettingBinding
 
 class BeaconSettingFragment : Fragment(){
-    fun onCreateView(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?{
+        val binding: FragmentBeaconSettingBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragmen_beacon_setting,
+            container,
+            false
+        )
 
+        val list = UserDataRepository.findAll()
+
+        binding.also{ it ->
+            it.setLifecycleOwner(this)
         }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/beacon/BeaconSettingViewModel.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/setting/beacon/BeaconSettingViewModel.kt
@@ -1,10 +1,10 @@
 package com.ict.ryokon.shugakuryokon2018.ui.setting.beacon
 
+import android.os.Bundle
 import androidx.lifecycle.ViewModel
 
 class BeaconSettingViewModel : ViewModel(){
-    fun removeOnClick(){
-
+    fun removeOnClick(savedInstanceState: Bundle?){
 
 
     }
