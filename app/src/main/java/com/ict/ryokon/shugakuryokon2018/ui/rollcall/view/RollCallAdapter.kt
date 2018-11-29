d799ad0f1a3668e5a8d4fcf8ba064e38ef33ae42
mitohato <mitohato14@gmail.com>
2018-11-29 14:18:34 +0900

    Add callback by change

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/rollcall/view/RollCallAdapter.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/rollcall/view/RollCallAdapter.kt
@@ -3,11 +3,65 @@ package com.ict.ryokon.shugakuryokon2018.ui.rollcall.view
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableList
 import androidx.recyclerview.widget.RecyclerView
 import com.ict.ryokon.shugakuryokon2018.model.UserData
 
 class RollCallAdapter(private val userDataList: ObservableArrayList<UserData>) :
     RecyclerView.Adapter<RollCallViewHolder>() {
+
+    init {
+        userDataList.addOnListChangedCallback(object :
+            ObservableList.OnListChangedCallback<ObservableArrayList<UserData>>() {
+            override fun onChanged(sender: ObservableArrayList<UserData>?) {
+                notifyDataSetChanged()
+            }
+
+            override fun onItemRangeRemoved(
+                sender: ObservableArrayList<UserData>?,
+                positionStart: Int,
+                itemCount: Int
+            ) {
+                notifyItemRangeRemoved(positionStart, itemCount)
+            }
+
+            override fun onItemRangeMoved(
+                sender: ObservableArrayList<UserData>?,
+                fromPosition: Int,
+                toPosition: Int,
+                itemCount: Int
+            ) {
+                for (i in 0..itemCount) {
+                    notifyItemMoved(
+                        fromPosition + i,
+                        toPosition + i
+                    )
+                }
+            }
+
+            override fun onItemRangeInserted(
+                sender: ObservableArrayList<UserData>?,
+                positionStart: Int,
+                itemCount: Int
+            ) {
+                notifyItemRangeInserted(
+                    positionStart,
+                    itemCount
+                )
+            }
+
+            override fun onItemRangeChanged(
+                sender: ObservableArrayList<UserData>?,
+                positionStart: Int,
+                itemCount: Int
+            ) {
+                notifyItemRangeChanged(
+                    positionStart,
+                    itemCount
+                )
+            }
+        })
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
