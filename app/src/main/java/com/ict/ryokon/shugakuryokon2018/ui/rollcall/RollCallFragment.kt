5cc00a98c29d8f7b2bf1c768924de9cba13f5947
mitohato <mitohato14@gmail.com>
2018-11-29 13:10:13 +0900

    To pricate

--- app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/rollcall/RollCallFragment.kt
+++ app/src/main/java/com/ict/ryokon/shugakuryokon2018/ui/rollcall/RollCallFragment.kt
@@ -146,6 +146,6 @@ class RollCallFragment : Fragment(), BeaconConsumer {
     }
 
     companion object {
-        const val IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
+        private const val IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
     }
 }
